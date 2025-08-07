12571d87aa1b10026ebc2ad420d5dfe5e45886c8
fujino-motoki7893 <motoki.fujino7893@gmail.com>
2025-08-07 16:32:06 +0900

    Revert "✨ 技術スタックにJava 追加 (#78)" (#79)
    This reverts commit 52271cfaa1ef81a72a9333d2d527a84af31d0987.

--- components/layouts/About.vue
+++ components/layouts/About.vue
@@ -164,8 +164,7 @@
             バックエンド...
           </span>
           <p class="w-full lg:w-[1220px]">
-            C#, ASP.NET Core, GraphQL, REST, Swagger, Nitro, クリーンアーキテクチャ, Repository pattern, Jet Brains Rider,
-            Java（C#との親和性が高いことから、Spring Boot を用いて学習中）
+            C#, ASP.NET Core, GraphQL, REST, Swagger, Nitro, クリーンアーキテクチャ, Repository pattern, Jet Brains Rider
           </p>
         </div>
         <div class="flex flex-col lg:flex-row mb-6 lg:mb-8">
