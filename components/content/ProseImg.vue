80ca4d2aedb02fbb95912399ab1f6753fb23f1f3
fujino-motoki22-fixer <fujino.motoki22@fixer.co.jp>
2026-02-09 10:52:49 +0900

    💄記事で画像を表示できるように

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,28 @@
+<template>
+  <img
+    :src="imageSrc"
+    :alt="alt"
+    :width="width"
+    :height="height"
+    loading="lazy"
+    class="rounded-lg"
+  >
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  src: string
+  alt?: string
+  width?: string | number
+  height?: string | number
+}>()
+
+const imageSrc = computed(() => {
+  // 既に絶対パスなら、そのまま使用
+  if (props.src.startsWith('/')) {
+    return props.src
+  }
+  // 相対パスなら、/images/をプレフィックスとして追加
+  return `/images/${props.src}`
+})
+</script>
